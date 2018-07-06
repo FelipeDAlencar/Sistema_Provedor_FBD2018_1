2f5bc086069cb96c28711e1f70c570c30b82c867
FilipeAlv <lfilipealves29@gmail.com>
2018-07-06 16:31:37 -0300

    0.4.6

---
+++ src/br/com/sistema_provedor_fbd_2018_1/controller/ControllerEditarServicoCliente.java
@@ -0,0 +1,65 @@
+package br.com.sistema_provedor_fbd_2018_1.controller;
+
+import java.awt.event.ActionEvent;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import br.com.sistema_provedor_fbd_2018_1.entidade.Cidade;
+import br.com.sistema_provedor_fbd_2018_1.entidade.Endereco;
+import br.com.sistema_provedor_fbd_2018_1.entidade.Porta;
+import br.com.sistema_provedor_fbd_2018_1.entidade.Servico;
+import br.com.sistema_provedor_fbd_2018_1.entidade.ServicoCliente;
+import br.com.sistema_provedor_fbd_2018_1.entidade.Switch;
+import br.com.sistema_provedor_fbd_2018_1.exception.BusinessException;
+import br.com.sistema_provedor_fbd_2018_1.fachada.Fachada;
+import br.com.sistema_provedor_fbd_2018_1.fachada.IFachada;
+import br.com.sistema_provedor_fbd_2018_1.model.Listeners;
+import br.com.sistema_provedor_fbd_2018_1.view.InternalEditarServicoCliente;
+
+public class ControllerEditarServicoCliente  implements Listeners{
+	private InternalEditarServicoCliente internalEditarServicoCliente;
+	private ServicoCliente servicoCliente;
+	public ControllerEditarServicoCliente(ServicoCliente servico) {
+		this.servicoCliente = servico;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == internalEditarServicoCliente.getBtnSalvar()) {
+			
+		}
+	}
+
+	@Override
+	public void addListeners() {
+		internalEditarServicoCliente.getBtnSalvar().addActionListener(this);
+	}
+	
+	public void setInternalEditarServicoCliente(InternalEditarServicoCliente internalEditarServicoCliente) {
+		this.internalEditarServicoCliente = internalEditarServicoCliente;
+	}
+	
+	public void preencherCampos() {
+		IFachada fachada = new Fachada();
+		try {
+		Endereco endereco = fachada.buscarEnderecoPorId(servicoCliente.getEndereco_id());
+		Switch sw = fachada.buscarSwitchPorId(servicoCliente.getSwitch_id());
+		Porta porta = fachada.buscarPortaPorId(servicoCliente.getPorta_id());
+		Servico servico = fachada.buscarServicoPorId(servicoCliente.getServico_id());
+	
+		internalEditarServicoCliente.getBairroField().setText(endereco.getBairro());
+		internalEditarServicoCliente.getComplementoField().setText(endereco.getComplemento());
+		internalEditarServicoCliente.getNumeroField().setText(String.valueOf(endereco.getNumero()));
+		internalEditarServicoCliente.getRuaField().setText(endereco.getRua());
+		internalEditarServicoCliente.getLoginField().setText(servicoCliente.getLogin());
+		internalEditarServicoCliente.getSenhaField().setText(servicoCliente.getSenha());
+		internalEditarServicoCliente.getPortaComboBox().setSelectedItem(porta.getNumero());
+		internalEditarServicoCliente.getServicosComboBox().setSelectedItem(servico.getNome());
+		internalEditarServicoCliente.getSwitchComboBox().setSelectedItem(sw.getNome());
+		
+		
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/br/com/sistema_provedor_fbd_2018_1/view/InternalEditarServicoCliente.java
@@ -0,0 +1,18 @@
+package br.com.sistema_provedor_fbd_2018_1.view;
+
+import java.awt.event.ActionListener;
+
+import br.com.sistema_provedor_fbd_2018_1.exception.BusinessException;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+
+public class InternalEditarServicoCliente extends InternalAdicionarServico{
+
+	public InternalEditarServicoCliente(TelaPrincipal telaPrincipal, ActionListener actionListener)
+			throws BusinessException {
+		super(telaPrincipal, actionListener);
+		setTitle("Editar Serviço");
+	}
+
+}
+
